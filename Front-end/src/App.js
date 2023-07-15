53d67f2fbba70e4a00a9b0a0651ac1d5ac0fe155
Saif-Azam-Khan <saif.azamkhan77@gmail.com>
2023-07-16 03:23:03 +0530

    removed some dependencies and added comments and renamed a function

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,11 +1,9 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Cards from "./components/Cards";
-
 import DetailPage from "./components/DetailPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { NEW } from "./redux/actions/action";
 import "./App.css";
@@ -17,23 +15,29 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    axios.get(BASE_URL).then((res) => {
-      setDatas(JSON.parse(res.data));
-      console.log(datas)
-      console.log("123");
-    });
+    // Fetch data from the specified BASE_URL
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        // Set the retrieved data to the 'datas' state
+        setDatas(data);
+      });
   }, []);
-   
- const abc= ()=>{
-    console.log(datas)
-    dispatch(NEW(datas))
-  }
+
+  const storNewData = () => {
+    console.log("in abc");
+    // Dispatch the 'NEW' action with the 'datas' as payload
+    dispatch(NEW(datas));
+  };
 
   return (
     <div className="App">
-      <NavBar onClick={abc()}/>
+      {/* Render the NavBar component and pass the 'storNewData' function as a prop */}
+      <NavBar onClick={storNewData()} />
       <Routes>
-        <Route path="/" element={<Cards/>} />
+        {/* Render the Cards component and pass the 'datas' as a prop */}
+        <Route path="/" element={<Cards datas={datas} />} />
+        {/* Render the DetailPage component */}
         <Route path="/details/:id" element={<DetailPage />} />
       </Routes>
     </div>
